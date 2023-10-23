818be805f207e6fb49acb6ec4b3fd8f9f035bc78
ar-ray0111 <arjunomray@gmail.com>
2023-10-23 17:20:28 +0530

    first commit

---
+++ lib/View/Register_view.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_2/constants/routes.dart';
+import 'dart:developer'as devtools show log;
+
+import '../firebase_options.dart';
+import '../utilities/show_error._dialog.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  @override
+  void initState() {
+    _email=TextEditingController();
+    _password=TextEditingController();
+    super.initState();
+  }
+  @override
+  void dispose() {
+   _email.dispose();
+   _password.dispose();
+
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    appBar: AppBar(title: const Text('Register'),),
+     body: Column(
+          children: [
+            TextField(
+             controller: _email,
+             autocorrect: false,
+             enableSuggestions: false,
+             keyboardType: TextInputType.emailAddress,
+               decoration: const InputDecoration(
+                hintText: 'Enter your email here',
+              ),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              autocorrect: false,
+              enableSuggestions: false,
+              decoration: const InputDecoration(
+                hintText: 'Enter your password here',
+              ),
+            
+            ),
+      
+            TextButton(onPressed: ()  {
+             Navigator.of(context).pushNamedAndRemoveUntil(
+                  notesroute, 
+                 (route) => false,);
+      
+            },
+            child: const Text('Register'),),
+            TextButton(onPressed:() {
+               Navigator.of(context).pushNamedAndRemoveUntil(loginroute, 
+            (route) => false,);
+            }, 
+            child: const Text('Already registered? login here!'))
+          ],
+        ),
+   );
+  }
+}
+
